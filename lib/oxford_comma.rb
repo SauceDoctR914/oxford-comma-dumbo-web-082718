a7834d8de6a9c33ac237db9a1a8c023e78432897
Gavin Soko <sokolofg@gmail.com>
2018-08-14 05:37:44 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+newS = array.slice(-1)
+array.delete_at(-1)
+newGuy = newS.insert(0, ", and ")
+newGuy.to_s
+newArray = array.join(", ")
+if  array.length > 2
+return newArray + newGuy
+else 
+  
 end
